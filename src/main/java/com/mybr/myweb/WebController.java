52cafa9e9df06ea86bc6466124450cb4b54077e6
HEILY\da003 <da003321@naver.com>
2023-09-14 15:42:28 +0900

    할루

---
+++ src/main/java/com/mybr/myweb/WebController.java
@@ -0,0 +1,24 @@
+package com.mybr.myweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/web")
+public class WebController {
+	@GetMapping("")
+	public String menu() {
+		return "menu";
+	}
+	
+	@GetMapping("book")
+	public String book() {
+		return "book";
+	}
+	
+	@GetMapping("cert")
+	public String cert() {
+		return "cert";
+	}
+}
---
+++ src/main/java/com/mybr/myweb/service/ComService.java
@@ -0,0 +1,10 @@
+package com.mybr.myweb.service;
+
+import java.util.List;
+
+import com.mybr.myweb.dto.ComDTO;
+
+public interface ComService {
+	public List<ComDTO> list();
+	public ComDTO detail(int id);
+}
